af07a08252414fee36401d31ee9d3d8bb10950d0
TDLive <thunderlive-sunshine@live.com>
2013-04-22 17:46:30 -0400

    Fonts better <3

--- lib/taurus/taurus.php
+++ lib/taurus/taurus.php
@@ -60,7 +60,7 @@ class Taurus {
 		<title>" . constant("TAURUS_NAME") . ' / ' . constant("TAURUS_LOG_IN") . '</title>
 	</head>
 	<body>
-		<center><p align="center" style="font-face: Bubblegum Sans;"><h1>' . constant("TAURUS_NAME") . '</h1><b><i>' . constant("TAURUS_MOTTO") . '</i></b></p><h2>' . constant("TAURUS_LOG_IN") . '</h2><br>';
+		<center><p align="center" style="font-family: ' . '"Bubblegum Sans"' . ', cursive;"><h1>' . constant("TAURUS_NAME") . '</h1><b><i>' . constant("TAURUS_MOTTO") . '</i></b></p><h2>' . constant("TAURUS_LOG_IN") . '</h2><br>';
 		if(isset($error)) {
 			echo '<font color="red">' . $error . '</font>';
 		}
