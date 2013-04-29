9545e9ac21da1c9cf77389584a33aa9a7e536a49
TDLive <thunderlive-sunshine@live.com>
2013-04-29 15:55:43 -0400

    If you can't beat 'em, join 'em.

--- lib/taurus/taurus.php
+++ lib/taurus/taurus.php
@@ -18,7 +18,6 @@
 */
 
 require('lib/facebook/facebook.php');
-require('lib/taurus/pageengine.php');
 require('lib/phpass/PasswordHash.php');
 
 class Taurus {
@@ -186,5 +185,34 @@ navbar {
 		$this->fb_firstname = $user['first_name'];
 		$this->fb_lastname = $user['last_name'];
 		return;
+	}
+}
+
+class PagesEngine {
+	public $page;
+	function __construct($page){
+		if(! file_exists("../../pages/$page.page")){
+			die("TranslationsEngine: Page $page does NOT exist.");
+		}
+		$this->page = $page;
+	}
+	function engine(){
+		if(! $contents=file_get_contents("../../pages/" . $this->page . ".page")){
+			die("TranslationsEngine: Cannot access " . $this->page . ".");
+		}
+		#Here comes the fun part
+		$contents=str_replace("{{TAURUS_NAME}}", constant("TAURUS_NAME"), $contents);
+		$contents=str_replace("{{TAURUS_LOG_IN}}", constant("TAURUS_LOG_IN"), $contents);
+		$contents=str_replace("{{TAURUS_NAME}}", constant("TAURUS_NAME"), $contents);
+		$contents=str_replace("{{TAURUS_LOG_IN_INCORRECT}}", constant("TAURUS_LOG_IN_INCORRECT"), $contents);
+		$contents=str_replace("{{TAURUS_LOG_IN_INCOMPLETE}}", constant("TAURUS_LOG_IN_INCOMPLETE"), $contents);
+		$contents=str_replace("{{TAURUS_MOTTO}}", constant("TAURUS_MOTTO"), $contents);
+		$contents=str_replace("{{TAURUS_HOME}}", constant("TAURUS_HOME"), $contents);
+		$contents=str_replace("{{TAURUS_404TEXT}}", constant("TAURUS_LOG_IN_INCORRECT"), $contents);
+		$contents=str_replace("{{TAURUS_404LINK}}", constant("TAURUS_LOG_IN_INCORRECT"), $contents);
+		$contents=str_replace("{{TAURUS_LOGOUT}}", constant("TAURUS_LOG_IN_INCORRECT"), $contents);
+		if(@isset(Taurus::$username)){
+			$contents=str_replace("{{USERNAME}}", Taurus::$username, $contents);
+		}
 	}
 }
